aa696a3d5c1825ffed0d35090b2642022cf6b77a
Letícia Xavier <lsx@cin.ufpe.br>
2026-05-14 17:08:07 -0300

    Refactor timer_sleep to use thread_sleep
    Removed busy wait in timer_sleep and added thread_sleep for improved efficiency.

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -24,6 +24,8 @@ static int64_t ticks;
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
+static struct list sleep_list; // declarando a lista dos bloqueados (dormindo) 
+
 static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
@@ -35,6 +37,7 @@ static void real_time_delay (int64_t num, int32_t denom);
 void
 timer_init (void) 
 {
+  list_init(&sleep_list); // inicia lista
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
@@ -92,8 +95,10 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  // remove a busy wait 
+  if (timer_elapsed (start) < ticks) 
+  {thread_sleep(start + ticks);};
+  
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be

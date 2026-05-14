4d40409a8d4e586eb429c2cbda71a71e2908355e
Letícia Xavier <lsx@cin.ufpe.br>
2026-05-14 17:10:08 -0300

    Add wakeup_tick to thread structure

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -83,6 +83,7 @@ typedef int tid_t;
 struct thread
   {
     /* Owned by thread.c. */
+    int64_t wakeup_tick;                // guarda o tempo para acordar
     tid_t tid;                          /* Thread identifier. */
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
